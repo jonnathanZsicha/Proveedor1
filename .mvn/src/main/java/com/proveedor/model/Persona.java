0b688a60be409151004ef5a3c82ddda092234eeb
jonnathanZsicha <jsichar@est.ups.edu.ec>
2021-05-17 22:10:06 -0500

    base de datos

--- .mvn/src/main/java/com/proveedor/model/Persona.java
+++ .mvn/src/main/java/com/proveedor/model/Persona.java
@@ -3,6 +3,7 @@ package com.proveedor.model;
 import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -13,6 +14,7 @@ import lombok.Data;
 @Data public class Persona {
 	
 	@Id
+	@GeneratedValue
 	private int id;
 	private String nombre ;
 	private String apellido;
